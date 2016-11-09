12cbe108200c6a1db60e8fb9c5ccb1f680f299b1
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-09 21:43:46 +0100

    Added simple list view

--- app/src/main/java/johannes/playground/PgActivityListView.java
+++ app/src/main/java/johannes/playground/PgActivityListView.java
@@ -3,16 +3,37 @@ package johannes.playground;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by johannesklein on 09.11.16.
  */
 public class PgActivityListView extends AppCompatActivity {
 
+    private String[] mData = new String[]{
+            "test1",
+            "test2",
+            "test3",
+            "test4",
+            "test5",
+            "test6",
+    };
+
+    private ListView mListView = null;
+    private ArrayAdapter<String> mAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_list_view);
 
+        mListView = (ListView) findViewById(R.id.listView);
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mData);
+
+        if (mAdapter != null){
+            mListView.setAdapter(mAdapter);
+        }
     }
 }
