6b4175861ebc69dd96744bdaced01e008a3d2a39
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-09 15:16:31 +0100

    Added Button to Intents activity

--- app/src/main/java/johannes/playground/PgActivityIntents.java
+++ app/src/main/java/johannes/playground/PgActivityIntents.java
@@ -7,9 +7,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class PgActivityIntents extends AppCompatActivity {
+
+    private Button mButton = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_intents);
+
+        mButton = (Button) findViewById(R.id.buttonImplicitIntents);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(view.getContext(), PgActivityImplicitIntents.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
