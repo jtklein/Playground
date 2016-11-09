afb8679aee3d598e3150ff0b1cd895aed37eb601
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-09 17:38:06 +0100

    Directed button to lifecycle playground

--- app/src/main/java/johannes/playground/PgActivityActivities.java
+++ app/src/main/java/johannes/playground/PgActivityActivities.java
@@ -1,13 +1,29 @@
 package johannes.playground;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PgActivityActivities extends AppCompatActivity {
+
+    private Button mButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_activities);
+
+        // Go to activity lifecycle
+        mButton = (Button) findViewById(R.id.buttonActivityLifecycle);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Simple intent
+                Intent intent = new Intent(view.getContext(), PgActivityLifecycle.class);
+                startActivity(intent);
+            }
+        });
     }
 }
