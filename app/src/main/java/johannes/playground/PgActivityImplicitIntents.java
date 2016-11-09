baf4a511d6ad06c0720cb12153893d734f3ceeb6
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-09 14:58:41 +0100

    Added Spinner to Implicit Intents Activity

--- app/src/main/java/johannes/playground/PgActivityImplicitIntents.java
+++ app/src/main/java/johannes/playground/PgActivityImplicitIntents.java
@@ -2,15 +2,20 @@ package johannes.playground;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Spinner;
 
 /**
  * Created by johannesklein on 09.11.16.
  */
 public class PgActivityImplicitIntents extends AppCompatActivity {
 
+    private Spinner mSpinner = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_implicit_intents);
+
+        mSpinner = (Spinner) findViewById(R.id.spinnerImplicitIntents);
     }
 }
