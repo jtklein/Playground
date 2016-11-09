1aafdbdfd31caab8fa1fa1c79178eacdafa13bda
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-09 16:13:24 +0100

    Added basic receiving intent extra activity

--- app/src/main/java/johannes/playground/PgActivityReceiveIntentExtras.java
+++ app/src/main/java/johannes/playground/PgActivityReceiveIntentExtras.java
@@ -1,7 +1,18 @@
 package johannes.playground;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by johannesklein on 09.11.16.
  */
-public class PgActivityReceiveIntentExtras {
+public class PgActivityReceiveIntentExtras extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_receive_intent_extras);
+
+    }
 }
