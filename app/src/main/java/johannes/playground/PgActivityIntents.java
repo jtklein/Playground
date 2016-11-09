bb66ee570941025163289243135543f959457b5f
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-09 15:15:52 +0100

    Added Activity for different Intents

---
+++ app/src/main/java/johannes/playground/PgActivityIntents.java
@@ -0,0 +1,15 @@
+package johannes.playground;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PgActivityIntents extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_intents);
+    }
+}
