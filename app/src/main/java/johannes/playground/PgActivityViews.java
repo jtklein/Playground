d4d0e0e394fad19b7f90b73c1e376e5aa1f81705
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-09 21:17:10 +0100

    Added basic activity for view playground

---
+++ app/src/main/java/johannes/playground/PgActivityViews.java
@@ -0,0 +1,33 @@
+package johannes.playground;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by johannesklein on 09.11.16.
+ */
+public class PgActivityViews extends AppCompatActivity {
+
+    private Button mButton = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_views);
+
+        mButton = (Button) findViewById(R.id.buttonListView);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Simple Explicit Intent
+                Intent intent = new Intent(view.getContext(), PgActivityListView.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
