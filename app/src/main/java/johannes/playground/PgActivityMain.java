d7c99f05852ef8e3b9ad57059a9fc84167518ee7
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-09 17:32:09 +0100

    Added button to go to activities playground

--- app/src/main/java/johannes/playground/PgActivityMain.java
+++ app/src/main/java/johannes/playground/PgActivityMain.java
@@ -8,19 +8,31 @@ import android.widget.Button;
 
 public class PgActivityMain extends AppCompatActivity {
 
-    private Button mButton = null;
+    private Button mButtonActivities = null;
+    private Button mButtonIntents = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_main);
 
-        mButton = (Button) findViewById(R.id.buttonIntents);
-
-        mButton.setOnClickListener(new View.OnClickListener() {
+        // The button to go to activities playground
+        mButtonActivities = (Button) findViewById(R.id.buttonActivities);
+        mButtonActivities.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Simple intent
+                Intent intent = new Intent(view.getContext(), PgActivityActivities.class);
+                startActivity(intent);
+            }
+        });
 
+        // The button to go to intents playground
+        mButtonIntents = (Button) findViewById(R.id.buttonIntents);
+        mButtonIntents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Simple explicit intent
                 Intent intent = new Intent(view.getContext(), PgActivityIntents.class);
                 startActivity(intent);
             }
