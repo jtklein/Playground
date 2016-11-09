3c1cca0ca49baa5b7adf8dee24eda1f86aabeb47
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-09 16:12:42 +0100

    Added EditText and Button to send intent activity

--- app/src/main/java/johannes/playground/PgActivitySendIntentExtras.java
+++ app/src/main/java/johannes/playground/PgActivitySendIntentExtras.java
@@ -3,16 +3,30 @@ package johannes.playground;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by johannesklein on 09.11.16.
  */
 public class PgActivitySendIntentExtras extends AppCompatActivity {
 
+    private EditText mEditText = null;
+    private Button mButton = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_send_intent_extras);
 
+        mEditText = (EditText) findViewById(R.id.editTextSendIntentExtraInt);
+        mButton = (Button) findViewById(R.id.buttonSendIntentExtraInt);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }
