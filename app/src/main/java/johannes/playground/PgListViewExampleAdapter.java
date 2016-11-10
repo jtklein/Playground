8fb5ae0331345d3ddfdd5d9b71ecd89f3a876834
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-10 14:37:23 +0100

    Added custom Array Adapter for new data class

---
+++ app/src/main/java/johannes/playground/PgListViewExampleAdapter.java
@@ -0,0 +1,16 @@
+package johannes.playground;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by johannesklein on 10.11.16.
+ */
+public class PgListViewExampleAdapter extends ArrayAdapter<PgListViewExampleItem> {
+}
