edc867f78cd36b8ff90b1e28ce76d18c2578e35e
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-10 15:16:45 +0100

    Added simple activity for persistence playground

---
+++ app/src/main/java/johannes/playground/PgActivityPersistance.java
@@ -0,0 +1,18 @@
+package johannes.playground;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by johannesklein on 10.11.16.
+ */
+public class PgActivityPersistance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_persistance);
+
+    }
+}
