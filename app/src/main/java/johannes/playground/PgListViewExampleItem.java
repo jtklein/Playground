168e6530c916437fab4cca769235048e844a6c9a
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-10 14:35:27 +0100

    Added basic class for list view item

---
+++ app/src/main/java/johannes/playground/PgListViewExampleItem.java
@@ -0,0 +1,19 @@
+package johannes.playground;
+
+/**
+ * Created by johannesklein on 10.11.16.
+ */
+
+public class PgListViewExampleItem {
+
+    public String header;
+    public String body;
+    public String popup;
+
+    public PgListViewExampleItem(String header, String body, String popup) {
+
+        this.header = header;
+        this.body = body;
+        this.popup = popup;
+    }
+}
