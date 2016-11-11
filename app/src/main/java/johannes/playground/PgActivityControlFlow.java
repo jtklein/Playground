764140cd6992523d5f1fae098dfd80c8822f94c5
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-11 19:12:19 +0100

    Added basic control flow activity

---
+++ app/src/main/java/johannes/playground/PgActivityControlFlow.java
@@ -0,0 +1,17 @@
+package johannes.playground;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by johannesklein on 11.11.16.
+ */
+public class PgActivityControlFlow extends PgActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.pg_activity_control_flow);
+
+    }
+}
