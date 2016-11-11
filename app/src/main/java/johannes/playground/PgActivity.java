dd2478f82644a257605c802b54106cd8a232e0ec
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-11 14:46:21 +0100

    Added activity superclass

---
+++ app/src/main/java/johannes/playground/PgActivity.java
@@ -0,0 +1,12 @@
+package johannes.playground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+/**
+ * Created by johannesklein on 11.11.16.
+ */
+
+public class PgActivity extends AppCompatActivity {
+
+}
