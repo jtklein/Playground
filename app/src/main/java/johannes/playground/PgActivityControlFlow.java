bde8593a27a13fada6156606a2f5c7c5463432b0
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-12 00:58:28 +0100

    Added layout elements to control flow activity

--- app/src/main/java/johannes/playground/PgActivityControlFlow.java
+++ app/src/main/java/johannes/playground/PgActivityControlFlow.java
@@ -1,17 +1,33 @@
 package johannes.playground;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * Created by johannesklein on 11.11.16.
  */
 public class PgActivityControlFlow extends PgActivity {
 
+    private Button mButton = null;
+    private Spinner mSpinner = null;
+    private EditText mEditText = null;
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_control_flow);
 
+        mButton = (Button) findViewById(R.id.buttonTryPangram);
+        mSpinner = (Spinner) findViewById(R.id.spinnerPangram);
+        mEditText = (EditText) findViewById(R.id.editTextPangram);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
     }
 }
