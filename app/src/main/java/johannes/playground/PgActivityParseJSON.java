daff36f32c295793185a5832120a2cd182cbdc14
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-11 01:29:05 +0100

    Added spinner to parse Json

--- app/src/main/java/johannes/playground/PgActivityParseJSON.java
+++ app/src/main/java/johannes/playground/PgActivityParseJSON.java
@@ -3,15 +3,23 @@ package johannes.playground;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Spinner;
 
 /**
  * Created by johannesklein on 10.11.16.
  */
 public class PgActivityParseJSON extends AppCompatActivity {
 
+    private Spinner mSpinner = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_parse_json);
+
+        // Get the spinner
+        mSpinner = (Spinner) findViewById(R.id.spinnerParseJSON);
+
+
     }
 }
