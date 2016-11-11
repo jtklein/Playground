8b95098caa61436f8c9b898eba5fba61d5e9b303
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-11 01:48:06 +0100

    Reading example son file from raw

--- app/src/main/java/johannes/playground/PgActivityParseJSON.java
+++ app/src/main/java/johannes/playground/PgActivityParseJSON.java
@@ -3,7 +3,11 @@ package johannes.playground;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Spinner;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by johannesklein on 10.11.16.
@@ -20,6 +24,32 @@ public class PgActivityParseJSON extends AppCompatActivity {
         // Get the spinner
         mSpinner = (Spinner) findViewById(R.id.spinnerParseJSON);
 
+        String exampleJSON = getJSONFile();
+    }
 
+    public String getJSONFile(){
+
+        String json = null;
+
+        try {
+            // Open an input stream
+            InputStream inputStream = getResources().openRawResource(R.raw.example_json);
+            int fileSize = 0;
+            fileSize = inputStream.available();
+            byte[] buffer = new byte[fileSize];
+            inputStream.read(buffer);
+            inputStream.close();
+
+            json = new String (buffer,"UTF-8");
+
+        } catch (IOException e) {
+            Log.e(this.getClass().getSimpleName(), "Error opening example_json file");
+            e.printStackTrace();
+            //TODO UX
+            return null;
+
+        }
+
+        return json;
     }
 }
