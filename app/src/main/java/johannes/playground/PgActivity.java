40742cd16980bd124f5e1c7e4e08d1d1857ade70
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-11 14:57:29 +0100

    Basic functionality for menu

--- app/src/main/java/johannes/playground/PgActivity.java
+++ app/src/main/java/johannes/playground/PgActivity.java
@@ -1,7 +1,10 @@
 package johannes.playground;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by johannesklein on 11.11.16.
@@ -14,6 +17,25 @@ public class PgActivity extends AppCompatActivity {
         // Inflate the menu
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //
+        switch (item.getItemId()){
+
+            case R.id.menuItemGoHome:
+                // Simple explicit intent to go to main
+                Intent intent = new Intent(this, PgActivityMain.class);
+                startActivity(intent);
+                return true;
+            case R.id.menuItemFingerprint:
+                Toast.makeText(this, "Nothing here yet!", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 }
