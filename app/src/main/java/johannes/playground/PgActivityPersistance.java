b8b1ff4b5aae49cb5db8cf86ad04bbc36578baed
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-11 02:23:29 +0100

    Added button to persistence activity

--- app/src/main/java/johannes/playground/PgActivityPersistance.java
+++ app/src/main/java/johannes/playground/PgActivityPersistance.java
@@ -1,18 +1,34 @@
 package johannes.playground;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by johannesklein on 10.11.16.
  */
 public class PgActivityPersistance extends AppCompatActivity {
 
+    private Button mButtonSharedPreferences = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_persistance);
+
+        // The button to go to shared preferences
+        mButtonSharedPreferences = (Button) findViewById(R.id.buttonSharedPreferences);
+        mButtonSharedPreferences.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Simple explicit intent
+                Intent intent = new Intent(view.getContext(), PgActivitySharedPreferences.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
