30d3a36ae5e022118d14005ddd53d5f2d3af3aff
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-12 01:28:43 +0100

    Added simple toast activity

---
+++ app/src/main/java/johannes/playground/PgActivityToast.java
@@ -0,0 +1,17 @@
+package johannes.playground;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by johannesklein on 12.11.16.
+ */
+public class PgActivityToast extends PgActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_toast);
+
+    }
+}
