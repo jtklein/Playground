dc78dbdcac90726fcb9232bb65c7f1a2e3087d84
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-12 13:22:29 +0100

    Added basic activity for intent requests

---
+++ app/src/main/java/johannes/playground/PgActivityIntentRequest.java
@@ -0,0 +1,19 @@
+package johannes.playground;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by johannesklein on 12.11.16.
+ */
+public class PgActivityIntentRequest extends PgActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_intent_request);
+    }
+}
