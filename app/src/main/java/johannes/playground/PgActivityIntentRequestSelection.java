d0b449459b7c27d50bd8eae47bfef54f019cc65c
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-12 13:24:13 +0100

    Added basic activity for intent request selection

---
+++ app/src/main/java/johannes/playground/PgActivityIntentRequestSelection.java
@@ -0,0 +1,16 @@
+package johannes.playground;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by johannesklein on 12.11.16.
+ */
+public class PgActivityIntentRequestSelection extends PgActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_intent_request_selection);
+    }
+}
