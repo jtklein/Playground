6753b9f25da9418d34beb61ba3f61c0ab5b40f88
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-12 13:22:59 +0100

    Added button to intent request

--- app/src/main/java/johannes/playground/PgActivityIntentRequest.java
+++ app/src/main/java/johannes/playground/PgActivityIntentRequest.java
@@ -11,9 +11,24 @@ import android.widget.Button;
  */
 public class PgActivityIntentRequest extends PgActivity {
 
+    private Button mButton = null;
+
+    private static final int REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_intent_request);
+
+        mButton = (Button) findViewById(R.id.buttonRequest);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+                Intent intent = new Intent(view.getContext(), PgActivityIntentRequestSelection.class);
+                startActivityForResult(intent, REQUEST_CODE);
+
+            }
+        });
     }
 }
