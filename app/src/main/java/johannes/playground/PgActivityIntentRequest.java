e1e7fe02b5c5cc0e86f6496996d8309f384223e1
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-12 13:41:10 +0100

    Get selection in onActivityResult and display

--- app/src/main/java/johannes/playground/PgActivityIntentRequest.java
+++ app/src/main/java/johannes/playground/PgActivityIntentRequest.java
@@ -5,20 +5,27 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by johannesklein on 12.11.16.
  */
 public class PgActivityIntentRequest extends PgActivity {
 
+    private TextView mTextView = null;
     private Button mButton = null;
 
     private static final int REQUEST_CODE = 1;
+    public static final String REQUEST_RETURN_KEY = "REQUEST_RETURN_KEY";
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_intent_request);
+
+        mTextView = (TextView) findViewById(R.id.textViewRequestSelection);
 
         mButton = (Button) findViewById(R.id.buttonRequest);
         mButton.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +37,23 @@ public class PgActivityIntentRequest extends PgActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE){
+
+            if (resultCode == RESULT_CANCELED){
+
+                Toast.makeText(this, "Selection canceled", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_OK){
+
+                if (data.hasExtra(REQUEST_RETURN_KEY)){
+
+                    String selection = data.getExtras().getString(REQUEST_RETURN_KEY);
+                    mTextView.setText(selection);
+                }
+            }
+        }
     }
 }
