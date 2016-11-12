093a666767298ac4c546feb54231239a72e12640
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-12 01:00:37 +0100

    Added method to test wether sentence is a pangram

--- app/src/main/java/johannes/playground/PgActivityControlFlow.java
+++ app/src/main/java/johannes/playground/PgActivityControlFlow.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by johannesklein on 11.11.16.
@@ -14,6 +15,9 @@ public class PgActivityControlFlow extends PgActivity {
     private Button mButton = null;
     private Spinner mSpinner = null;
     private EditText mEditText = null;
+
+    private static final char[] mAlphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +35,14 @@ public class PgActivityControlFlow extends PgActivity {
 
                     String pangram = mEditText.getText().toString();
                     char[] sentenceToTest = toCharacterArray(pangram);
+                    checkPangram(sentenceToTest);
+
                 } else {
 
                     String pangram = mSpinner.getSelectedItem().toString();
                     char[] sentenceToTest = toCharacterArray(pangram);
+                    checkPangram(sentenceToTest);
+
                 }
             }
         });
@@ -53,5 +61,32 @@ public class PgActivityControlFlow extends PgActivity {
         return charArray;
     }
 
+    private void checkPangram(char[] sentenceToTest) {
+
+        if (sentenceToTest.length == 0){
+
+            Toast.makeText(this, "Try writing one, or select one.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int lettersMissing = 0;
+
+        for (int i = 0; i < mAlphabet.length; i++){
+            for (int j = 0; j < sentenceToTest.length; j++){
+                if (mAlphabet[i] == sentenceToTest[j]){
+                    break;
+                } else if (j == sentenceToTest.length -1){
+                    lettersMissing++;
+                }
+            }
+        }
+
+        if (lettersMissing == 0){
+            Toast.makeText(this, "Yes it's a pangram!", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(this, "No, try again! " + lettersMissing + " letters missing.", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
