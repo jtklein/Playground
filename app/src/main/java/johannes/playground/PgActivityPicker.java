65784359b53867ae70a3c1adae8dd48987b5a610
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-13 16:37:22 +0100

    Added basic picker activity

---
+++ app/src/main/java/johannes/playground/PgActivityPicker.java
@@ -0,0 +1,17 @@
+package johannes.playground;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by johannesklein on 13.11.16.
+ */
+public class PgActivityPicker extends PgActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_picker);
+
+    }
+}
