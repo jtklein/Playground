cdd535286834b74922840762bf0b39f7761e728c
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-13 16:31:10 +0100

    Added button to user playground

--- app/src/main/java/johannes/playground/PgActivityUser.java
+++ app/src/main/java/johannes/playground/PgActivityUser.java
@@ -13,12 +13,14 @@ public class PgActivityUser extends PgActivity {
 
     private Button mButtonToast = null;
     private Button mButtonNotification = null;
+    private Button mButtonDialog = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_user);
 
+        // The button to go to Toast activity
         mButtonToast = (Button) findViewById(R.id.buttonToast);
         mButtonToast.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,12 +31,24 @@ public class PgActivityUser extends PgActivity {
             }
         });
 
+        // The button to go to notification activity
         mButtonNotification = (Button) findViewById(R.id.buttonNotification);
         mButtonNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Simple explicit intent
                 Intent intent = new Intent(view.getContext(), PgActivityNotification.class);
+                startActivity(intent);
+            }
+        });
+
+        // The button to go to dialog activity
+        mButtonDialog = (Button) findViewById(R.id.buttonDialog);
+        mButtonDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Simple explicit intent
+                Intent intent = new Intent(view.getContext(), PgActivityDialog.class);
                 startActivity(intent);
             }
         });
