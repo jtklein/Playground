96076a9690ed240a00b509b80c515815a2dda377
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-13 16:43:37 +0100

    Added basic networking activity

---
+++ app/src/main/java/johannes/playground/PgActivityNetworking.java
@@ -0,0 +1,16 @@
+package johannes.playground;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by johannesklein on 13.11.16.
+ */
+public class PgActivityNetworking extends PgActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_networking);
+    }
+}
