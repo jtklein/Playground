c82f2418cc34308bf0d66668b7ce4de3211474e6
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-16 14:06:07 +0100

    Added basic floating action button activity

---
+++ app/src/main/java/johannes/playground/PgActivityFloatingActionButton.java
@@ -0,0 +1,17 @@
+package johannes.playground;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by johannesklein on 15.11.16.
+ */
+public class PgActivityFloatingActionButton extends PgActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_floating_action_button);
+
+    }
+}
