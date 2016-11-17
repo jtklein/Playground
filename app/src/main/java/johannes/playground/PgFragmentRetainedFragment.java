cda46c5f750acf23f04b1acfe5bb403c2036af35
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-17 14:31:22 +0100

    Added simple AsyncTask to fragment

--- app/src/main/java/johannes/playground/PgFragmentRetainedFragment.java
+++ app/src/main/java/johannes/playground/PgFragmentRetainedFragment.java
@@ -1,6 +1,7 @@
 package johannes.playground;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 /**
@@ -11,10 +12,32 @@ public class PgFragmentRetainedFragment extends Fragment {
 
     public static final String TAG_RETAINED_FRAGMENT = "retained_fragment";
 
+    BackgroundTask mBackgroundTask = null;
+    /**
+     * Called once to do initial creation of a fragment.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setRetainInstance(true);
+    }
+    private class BackgroundTask extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            int progress = 0;
+            while(progress < 100 && !isCancelled()){
+                progress++;
+                publishProgress(progress);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
     }
 }
