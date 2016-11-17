75b68c95bc4ac699b2ae969aac8d2c2ac2ef3bc7
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-17 14:17:04 +0100

    Added elements to activity; save progress bar in lifecycle

--- app/src/main/java/johannes/playground/PgActivityRetainedFragment.java
+++ app/src/main/java/johannes/playground/PgActivityRetainedFragment.java
@@ -2,15 +2,72 @@ package johannes.playground;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by johannesklein on 17.11.16.
  */
 public class PgActivityRetainedFragment extends PgActivity {
 
+    private ProgressBar mProgressBar = null;
+    private TextView mProgressvalue = null;
+    private Button mButtonStart = null;
+    private Button mButtonCancel = null;
+    private Button mButtonRecreate = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_retained_fragment);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBarRetainedFragment);
+        mProgressvalue = (TextView) findViewById(R.id.textViewRetainedFragment);
+
+        // If activity is recreated get progress from saved instances
+        if (savedInstanceState != null) {
+            int progress = savedInstanceState.getInt("progress_value");
+            mProgressvalue.setText(progress + "%");
+            mProgressBar.setProgress(progress);
+        }
+
+        mButtonStart = (Button) findViewById(R.id.buttonStartRetainedFragment);
+        mButtonCancel = (Button) findViewById(R.id.buttonCancelRetainedFragment);
+        mButtonRecreate = (Button) findViewById(R.id.buttonRecreateRetainedFragment);
+
+        mButtonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mButtonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mButtonRecreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recreate();
+            }
+        });
+
     }
+
+    /**
+     * This method is called before an activity may be killed Store info in
+     * bundle if required.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("progress_value", mProgressBar.getProgress());
+    }
+
 }
