da06c4827edbbdd3743c68d00de9c99f8b3128b4
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-17 17:27:38 +0100

    Added listView to work history activity

--- app/src/main/java/johannes/playground/PgActivityWorkHistory.java
+++ app/src/main/java/johannes/playground/PgActivityWorkHistory.java
@@ -2,15 +2,32 @@ package johannes.playground;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ListView;
 
 /**
  * Created by johannesklein on 17.11.16.
  */
 public class PgActivityWorkHistory extends PgActivity {
 
+    private PgListViewWorkHistoryItem[] mData = new PgListViewWorkHistoryItem[]{
+            new PgListViewWorkHistoryItem("Date", "Job"),
+    };
+
+    private ListView mListView = null;
+
+    private PgListViewWorkHistoryAdapter mAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_work_history);
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        mAdapter = new PgListViewWorkHistoryAdapter(this, R.layout.pg_listview_item, mData);
+
+        if (mAdapter != null){
+            mListView.setAdapter(mAdapter);
+        }
     }
 }
