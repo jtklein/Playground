7fd28189358c848020a8f6b4fd8d1ec0a2f0d7c4
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-17 16:57:25 +0100

    Added simple work history activity

--- app/src/main/java/johannes/playground/PgActivityWorkHistory.java
+++ app/src/main/java/johannes/playground/PgActivityWorkHistory.java
@@ -1,7 +1,16 @@
 package johannes.playground;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by johannesklein on 17.11.16.
  */
-public class PgActivityWorkHistory {
+public class PgActivityWorkHistory extends PgActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_work_history);
+    }
 }
