d8a625faae125d7439ba3fa5c9c4e28a63777a65
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-18 16:43:55 +0100

    Added button menu item class

---
+++ app/src/main/java/johannes/playground/PgListViewButtonMenuItem.java
@@ -0,0 +1,17 @@
+package johannes.playground;
+
+/**
+ * Created by johannesklein on 10.11.16.
+ */
+
+public class PgListViewButtonMenuItem {
+
+    public String text;
+    public Class gotoClass;
+
+    public PgListViewButtonMenuItem(String text, Class gotoClass) {
+
+        this.text = text;
+        this.gotoClass = gotoClass;
+    }
+}
