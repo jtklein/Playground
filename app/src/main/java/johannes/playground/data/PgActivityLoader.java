1d003f3495f20bad0e35312b6aa7d80aae537ac2
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-18 21:43:34 +0100

    Added listView to Loader activity

--- app/src/main/java/johannes/playground/data/PgActivityLoader.java
+++ app/src/main/java/johannes/playground/data/PgActivityLoader.java
@@ -1,7 +1,18 @@
 package johannes.playground.data;
 
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.content.Loader;
 import android.os.Bundle;
+import android.os.FileObserver;
 import android.support.annotation.Nullable;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import johannes.playground.PgActivity;
 import johannes.playground.PgSimpleExplicitIntentFactory;
@@ -11,11 +22,16 @@ import johannes.playground.R;
  * Created by johannesklein on 18.11.16.
  */
 public class PgActivityLoader extends PgActivity {
+    // A RecyclerView.Adapter which will display the data
+    private ArrayAdapter<String> mAdapter;
+
+    private ListView mListView = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_loader);
 
+        mListView = (ListView) findViewById(R.id.listViewLoader);
     }
 }
