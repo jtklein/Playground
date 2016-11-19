851a31c7a1e9fc65a3df3b61147ddad5d4ce7ea9
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-19 17:03:34 +0100

    Added drop in animation to TicTacToe

--- app/src/main/java/johannes/playground/examples/tictactoe/PgActivityTicTacToe.java
+++ app/src/main/java/johannes/playground/examples/tictactoe/PgActivityTicTacToe.java
@@ -2,6 +2,8 @@ package johannes.playground.examples.tictactoe;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
 
 import johannes.playground.PgActivity;
 import johannes.playground.R;
@@ -11,10 +13,28 @@ import johannes.playground.R;
  */
 public class PgActivityTicTacToe extends PgActivity {
 
+    ImageView mImageView = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_tictactoe);
+
+    }
+
+
+    public void dropIn(View view) {
+
+        mImageView = (ImageView) view;
+
+        int tappedView = Integer.parseInt(mImageView.getTag().toString());
+
+        mImageView.setTranslationY(-1000f);
+
+        mImageView.setImageResource(R.drawable.red);
+
+        mImageView.animate().translationYBy(1000f).setDuration(400);
+
 
     }
 }
