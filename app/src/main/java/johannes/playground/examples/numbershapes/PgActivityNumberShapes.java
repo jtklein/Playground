f1b8060d082db2b5b9663645d99002fbc80becfe
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-19 02:53:28 +0100

    Added logic for number shapes

--- app/src/main/java/johannes/playground/examples/numbershapes/PgActivityNumberShapes.java
+++ app/src/main/java/johannes/playground/examples/numbershapes/PgActivityNumberShapes.java
@@ -22,4 +22,56 @@ public class PgActivityNumberShapes extends PgActivity {
 
     }
 
+    public void checkNumber(View view) {
+
+        EditText editText = (EditText) findViewById(R.id.editTextNumberShapes);
+
+        if (editText.getText().toString().isEmpty()){
+            
+        } else {
+
+            Number inputNumber = new Number();
+            inputNumber.number = Integer.parseInt(editText.getText().toString());
+
+            if (inputNumber.isSquare() && inputNumber.isTriangular()){
+
+                L.t(this, "Is triangular and square!");
+
+            } else if (inputNumber.isSquare()){
+                L.t(this, "Is square.");
+            } else if (inputNumber.isTriangular()){
+                L.t(this, "Is triangular.");
+            } else {
+                L.t(this, "Is neither.");
+            }
+        }
+    }
+
+    public static class Number{
+
+        int number;
+
+        public boolean isTriangular(){
+            int count = 0;
+            int triangularNumber = 0;
+            while (triangularNumber < number){
+                triangularNumber = triangularNumber + count;
+                count++;
+            }
+            if (triangularNumber == number) {
+                return true;
+            }
+            return false;
+        };
+
+        public boolean isSquare(){
+            double squareRoot = Math.sqrt(number);
+            if (squareRoot == Math.floor(squareRoot)){
+                return true;
+            }
+            return false;
+        };
+    }
+
+
 }
