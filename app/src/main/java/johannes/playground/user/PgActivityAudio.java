d64f1854c694f61d61bcfa25b33e34fd4015cb7d
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-19 21:16:15 +0100

    Added audio activity layout

--- app/src/main/java/johannes/playground/user/PgActivityAudio.java
+++ app/src/main/java/johannes/playground/user/PgActivityAudio.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 import johannes.playground.PgActivity;
 import johannes.playground.R;
@@ -12,12 +14,25 @@ import johannes.playground.R;
  * Created by johannesklein on 19.11.16.
  */
 public class PgActivityAudio extends PgActivity {
+
+    private Button mButtonPlay  = null;
+    private Button mButtonPause = null;
+
+    private SeekBar mSeekBarVolume = null;
+    private SeekBar mSeekBarScrubber = null;
+
     private MediaPlayer mMediaPlayer = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_audio);
+
+        mButtonPlay = (Button) findViewById(R.id.buttonPlayAudio);
+        mButtonPause = (Button) findViewById(R.id.buttonPauseAudio);
+
+        mSeekBarVolume = (SeekBar) findViewById(R.id.seekBarVolumeAudio);
+        mSeekBarScrubber = (SeekBar) findViewById(R.id.seekBarScrubAudio);
 
         // Get new MediaPlayer instance
         mMediaPlayer = MediaPlayer.create(this, R.raw.laugh);
