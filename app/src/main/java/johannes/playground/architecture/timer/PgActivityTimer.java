b81dc24bdeb81dc18ca992d2a5f65a9be16eb9d8
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-20 00:15:12 +0100

    Added Timer layout

--- app/src/main/java/johannes/playground/architecture/timer/PgActivityTimer.java
+++ app/src/main/java/johannes/playground/architecture/timer/PgActivityTimer.java
@@ -2,6 +2,8 @@ package johannes.playground.architecture.timer;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import johannes.playground.PgActivity;
 import johannes.playground.R;
@@ -11,9 +13,30 @@ import johannes.playground.R;
  */
 public class PgActivityTimer extends PgActivity {
 
+    private SeekBar mSeekBarDuration = null;
+    private SeekBar mSeekBarTick = null;
+
+    private TextView mTextViewDuration = null;
+    private TextView mTextViewTick = null;
+
+    private static final int minValueDuration = 2;
+    private static final int minValueTick = 1;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_timer);
+
+        mSeekBarDuration = (SeekBar) findViewById(R.id.seekBarDurationTimer);
+        mSeekBarTick = (SeekBar) findViewById(R.id.seekBarTickTimer);
+
+        mTextViewDuration = (TextView) findViewById(R.id.textViewDurationTimer);
+        mTextViewTick = (TextView) findViewById(R.id.textViewTickTimer);
+
+        mSeekBarDuration.setProgress(minValueDuration);
+        mSeekBarTick.setProgress(minValueTick);
+
+
     }
 }
