5917e02256df1c0e04bde0265181edf9756c9eb7
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-19 22:43:58 +0100

    Get filename through button ID and play audio

--- app/src/main/java/johannes/playground/examples/PgActivityPhraseBook.java
+++ app/src/main/java/johannes/playground/examples/PgActivityPhraseBook.java
@@ -1,5 +1,6 @@
 package johannes.playground.examples;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -12,6 +13,8 @@ import johannes.playground.R;
  */
 public class PgActivityPhraseBook extends PgActivity {
 
+    private MediaPlayer mMediaPlayer = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +22,16 @@ public class PgActivityPhraseBook extends PgActivity {
     }
 
     public void buttonTapped(View view) {
+
+        // Get filename from viewID, only works when button and raw files are homonym
+        int viewID = view.getId();
+        String resName = getResources().getResourceEntryName(viewID);
+        int resID = getResources().getIdentifier(resName, "raw", getPackageName());
+
+        // Start MediaPlayer for file
+        mMediaPlayer = MediaPlayer.create(this, resID);
+        mMediaPlayer.start();
+
+
     }
 }
