b17f5da54b74236e7723c69b024da31bf09e77e0
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-19 20:32:58 +0100

    Added video to activity

--- app/src/main/java/johannes/playground/user/PgActivityVideo.java
+++ app/src/main/java/johannes/playground/user/PgActivityVideo.java
@@ -2,6 +2,9 @@ package johannes.playground.user;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.VideoView;
+import android.widget.MediaController;
+
 
 import johannes.playground.PgActivity;
 import johannes.playground.R;
@@ -11,9 +14,27 @@ import johannes.playground.R;
  */
 public class PgActivityVideo extends PgActivity {
 
+    private VideoView mVideoView = null;
+
+    private MediaController mVideoController = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_video);
+
+        mVideoView = (VideoView) findViewById(R.id.videoViewVideo);
+
+        // Set video file path
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.demovideo;
+        mVideoView.setVideoPath(videoPath);
+
+        mVideoController = new MediaController(this);
+
+        // Tie together Video view and media controller
+        mVideoController.setAnchorView(mVideoView);
+        mVideoView.setMediaController(mVideoController);
+
+        mVideoView.start();
     }
 }
