9ba2586a9d56c491d81cc6399f86d5317f820e10
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-19 18:23:49 +0100

    If the field is empty no animation

--- app/src/main/java/johannes/playground/examples/tictactoe/PgActivityTicTacToe.java
+++ app/src/main/java/johannes/playground/examples/tictactoe/PgActivityTicTacToe.java
@@ -17,6 +17,12 @@ public class PgActivityTicTacToe extends PgActivity {
 
     private static boolean redIsPlaying = true;
 
+    // The status of the board initialized to 2 = all fields being unplaced
+    private static int[] mBoardStatus = {
+            2,2,2,
+            2,2,2,
+            2,2,2,
+    };
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,26 +35,35 @@ public class PgActivityTicTacToe extends PgActivity {
         // Get the clicked view
         mImageView = (ImageView) view;
 
-        // int tappedView = Integer.parseInt(mImageView.getTag().toString());
+        int tappedView = Integer.parseInt(mImageView.getTag().toString());
 
-        mImageView.setImageResource(whosPlaying());
+        if (!fieldIsEmpty(tappedView)) {
+            return;
+
+        }
 
         // Move image off screen and animate drop
+        mImageView.setImageResource(getPlayerImage());
         mImageView.setTranslationY(-1000f);
         mImageView.animate().translationYBy(1000f).setDuration(400);
 
 
     }
 
-    private int whosPlaying() {
-        // Check which player
+    private int getPlayerImage() {
+        // Get image for player
         if (redIsPlaying){
-            redIsPlaying = false;
             return R.drawable.red;
         }
-        redIsPlaying = true;
         return R.drawable.yellow;
     }
 
+
+    private boolean fieldIsEmpty(int field){
+        // Check if field is empty
+        if (mBoardStatus[field] == 2){
+            return true;
+        }
+        return false;
     }
 }
