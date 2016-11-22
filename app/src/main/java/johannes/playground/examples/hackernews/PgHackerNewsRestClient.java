23e0f705c39c63e546d1076312e0d2201e6ae029
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-22 22:15:11 +0100

    Added class for requesting son from hacker news api

---
+++ app/src/main/java/johannes/playground/examples/hackernews/PgHackerNewsRestClient.java
@@ -0,0 +1,28 @@
+package johannes.playground.examples.hackernews;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by johannesklein on 22.11.16.
+ */
+
+public class PgHackerNewsRestClient {
+
+    private static final String BASE_URL = "https://hacker-news.firebaseio.com/v0/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
