93ccfdf1e9cb672976aba51437005f992fbbf380
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-22 17:49:22 +0100

    Added web view activity

--- app/src/main/java/johannes/playground/views/PgActivityViews.java
+++ app/src/main/java/johannes/playground/views/PgActivityViews.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import johannes.playground.PgActivityButtonMenu;
 import johannes.playground.PgListViewButtonMenuItem;
 import johannes.playground.R;
+import johannes.playground.views.webview.PgActivityWebview;
 
 /**
  * Created by johannesklein on 09.11.16.
@@ -12,6 +13,7 @@ import johannes.playground.R;
 public class PgActivityViews extends PgActivityButtonMenu {
 
     private PgListViewButtonMenuItem[] mData = new PgListViewButtonMenuItem[]{
+            new PgListViewButtonMenuItem(R.string.ui_webview, PgActivityWebview.class),
             new PgListViewButtonMenuItem(R.string.ui_listview, PgActivityListView.class),
             new PgListViewButtonMenuItem(R.string.ui_fab, PgActivityFloatingActionButton.class),
     };
---
+++ app/src/main/java/johannes/playground/views/webview/PgActivityWebview.java
@@ -0,0 +1,42 @@
+package johannes.playground.views.webview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.Html;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import johannes.playground.PgActivity;
+import johannes.playground.R;
+
+/**
+ * Created by johannesklein on 22.11.16.
+ */
+public class PgActivityWebview extends PgActivity {
+
+    private WebView mWebView = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_activity_webview);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+
+        // Disable JavaScript
+        mWebView.getSettings().setJavaScriptEnabled(false);
+
+        // Set new webclient
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // Load Url
+        // mWebView.loadUrl("http://www.google.com");
+
+        // Build html string
+        String html = "<html><body><h1>Hi there!</h1><p>This is my website.</p></body></html>";
+
+        // Load html from data
+        mWebView.loadData(html, "text/html", "UTF-8");
+
+    }
+}
