7fbfe4feeb6b189c505f670579712c0218c62f92
Johannes Klein <johannes.t.klein@gmail.com>
2016-11-22 02:58:23 +0100

    Added basic SQL insert and query

--- app/src/main/java/johannes/playground/persistance/PgActivitySQLDatabase.java
+++ app/src/main/java/johannes/playground/persistance/PgActivitySQLDatabase.java
@@ -1,7 +1,10 @@
 package johannes.playground.persistance;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import johannes.playground.PgActivity;
 import johannes.playground.R;
@@ -16,5 +19,30 @@ public class PgActivitySQLDatabase extends PgActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_activity_sql_database);
 
+        try {
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users",MODE_PRIVATE, null);
+
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Me', 33)");
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('You', 32)");
+
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+
+            c.moveToFirst();
+            while (c != null){
+                // Get content
+                Log.i("name", c.getString(nameIndex));
+                Log.i("age", Integer.toString(c.getInt(ageIndex)));
+
+                c.moveToNext();
+            }
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
     }
 }
